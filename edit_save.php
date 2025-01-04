af61efea09634de64efe4b65c8d940a74888143e
Tawheedyahya <tawheedyahya0@gmail.com>
2025-01-04 12:13:21 +0530

    delete

--- edit_save.php
+++
@@ -1,8 +0,0 @@
-<?php
-if($_SERVER['REQUEST_METHOD']='POST'){
-    $conn = new mysqli("localhost", "root", "", "club");
-    $sql="UPDATE students set reg_no='{$row['reg_no']}' s_name='{$row['s_name']}' phone='{$row['phone']}' place='{$row['place']}' where id='{$row['id']}'";
-    mysqli_query($conn,$sql);
-    
-}
-?>
